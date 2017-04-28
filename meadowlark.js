ae001bf51dc2313632c31b19e7c591a507758237
rtaveras824 <rtaveras824@hotmail.com>
2017-04-27 20:19:13 -0400

    Handlebars render added to routes

--- meadowlark.js
+++ meadowlark.js
@@ -2,20 +2,31 @@ var express = require('express');
 
 var app = express();
 
+//set up handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
-//custom 404 page
-app.use(function(req, res) {
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not Found');
+app.get('/', function(req, res) {
+	res.render('home');
 });
 
-app.use(function(req, res) {
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+
+//custom 404 page
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next) {
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function() {
