c00c8f365c84be13499b25f8789a9069e3142fdc
rtaveras824 <rtaveras824@hotmail.com>
2017-04-29 08:19:30 -0400

    Added page test and cross page testing

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,6 @@
+// picks random fortune
 var fortune = require('./lib/fortune.js');
+
 var express = require('express');
 
 var app = express();
@@ -12,12 +14,28 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
 
 app.get('/about', function(req, res) {
-	res.render('about', { fortune: fortune.getFortune() });
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	 });
+});
+
+app.get('/tours/hood-river', function(req, res) {
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+	res.render('tours/request-group-rate');
 });
 
 //custom 404 page
